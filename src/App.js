70a78211e9304895ddba2b284f18ad2a9a759290
ChangBeumSeok <50513345+ChangBeumSeok@users.noreply.github.com>
2020-02-15 02:59:36 +0900

    Apply Material

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer'
+
+//import classes from '*.module.css';
+import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme =>({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX:"auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
 
 const customers = [
 {
@@ -30,26 +49,41 @@ const customers = [
 }
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return(
-            <Customer
-              key={c.id} // map을 사용하였기 때문에 key값을 지정해주어야함.
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              />
-          );
-        })
-      }
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {customers.map(c => {
+            return(
+              <Customer
+                key={c.id} // map을 사용하였기 때문에 key값을 지정해주어야함.
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                />
+            );
+          })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
